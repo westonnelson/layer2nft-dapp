04a633256503b086b1e1cbeea48b5ab5a986b5b0
Weston Nelson <westontnelson@outlook.com>
2022-12-26 22:38:24 -0600

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
 import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
@@ -38,5 +33,3 @@ export default function Home() {
     </section>
   );
 }
-
-const inter = Inter({ subsets: ['latin'] })
